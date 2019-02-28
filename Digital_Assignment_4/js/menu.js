3fd95d14e9a3f72264f595e2c1dc51e11160603e
Pikminfan1 <ryanprose96@gmail.com>
2019-02-28 13:16:23 -0500

    Update menu.js

--- Digital_Assignment_4/js/menu.js
+++ Digital_Assignment_4/js/menu.js
@@ -8,7 +8,7 @@ var menu = {
 
 
     create: function () {
-        game.add.text(160, 80, "Dog Can't stop thinking about those ghosts he saw last night\n and just wants to take a nap\nPress Enter to start", { fill: 'white', align: 'center'});
+        game.add.text(160, 80, "That Dog that Can't stop thinking about those ghosts he saw last night\n and really just needs to take a nap.\nPress Enter to start", { fill: 'white', align: 'center'});
         //game.add.text(160, 160, "Press Shift To Start With out Battle Royale", { fill: 'white' });
         enterKey = game.input.keyboard.addKey(Phaser.Keyboard.ENTER);
         shiftKey = game.input.keyboard.addKey(Phaser.Keyboard.SHIFT);
